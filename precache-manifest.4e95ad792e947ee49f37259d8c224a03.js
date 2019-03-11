275b704cd862004fbe40ed43a6e4c63da21d0168
hexadi <38806729+hexadi@users.noreply.github.com>
2019-03-11 16:13:37 +0700

    first!

---
+++ precache-manifest.4e95ad792e947ee49f37259d8c224a03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4d5bade3cd63feb0a642",
+    "url": "/js/chunk-vendors.a6697e29.js"
+  },
+  {
+    "revision": "a84b55cc0838fa5a6797",
+    "url": "/js/app.782a18af.js"
+  },
+  {
+    "revision": "dc48e2345d2d19a9a57fcd8ae349f8c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84b55cc0838fa5a6797",
+    "url": "/css/app.b8a20acf.css"
+  }
+];
